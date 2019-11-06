b61f7238baa5d5460a81b0bab158d74dbe687ead
tsdhr1 <gougewh@sina.com>
2019-11-06 11:42:24 +0800

    取消了图片加载的动画

--- js/1.js
+++ js/1.js
@@ -2,7 +2,7 @@ let img = document.querySelector('.img-load');
 let couver = document.querySelector('.couver');
 let infoBox = document.querySelector('.info-box');
 
-img.onload = () => {
-    couver.classList.add('left-show');
-    infoBox.classList.add('right-show')
-}
+// img.onload = () => {
+//     couver.classList.add('left-show');
+//     infoBox.classList.add('right-show')
+// }
