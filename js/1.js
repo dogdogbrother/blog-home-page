c24b11593dbcb04ac5a8e717eec871911a84e7c6
tsdhr1 <gougewh@sina.com>
2020-06-07 15:53:17 +0800

    添加了个轮播图

--- js/1.js
+++ js/1.js
@@ -2,7 +2,11 @@ let img = document.querySelector('.img-load');
 let couver = document.querySelector('.couver');
 let infoBox = document.querySelector('.info-box');
 
-// img.onload = () => {
-//     couver.classList.add('left-show');
-//     infoBox.classList.add('right-show')
-// }
+window.onload = () => {
+  var mySwiper = new Swiper ('.swiper-container', {
+    direction: 'vertical', // 垂直切换选项
+    loop: true, // 循环模式选项
+    effect: "fade",
+    autoplay: true
+  }) 
+}
